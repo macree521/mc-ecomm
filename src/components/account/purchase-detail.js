79fdaf37dfa5d8f2db2f5fcb53e97c22875f3123
Madison Creese <mads@Madisons-MacBook-Pro.local>
2020-12-28 09:29:07 -0800

    built user reducer with fetch purchases action creator to fetch user purchases data

--- src/components/account/purchase-detail.js
+++ src/components/account/purchase-detail.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class PurchaseDetail extends Component {
-  render() {
-      const { className } = this.props;
-    
-      return (
-        <div className={`${className} purchase-detail`}>
-          purchase details go here
-        </div>
-      )
+    render() {
+        const { className } = this.props;
+        
+        return (
+            <div className={`${className} purchase-detail`}>
+            purchase details go here
+            </div>
+        )
     }
 }
 
