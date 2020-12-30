141c77bfd5265371a9a142f3ab8a8dc403e03c25
Madison Creese <mads@Madisons-MacBook-Pro.local>
2020-12-30 13:28:50 -0800

    built account information grid and placed majority of fields on form grid

--- src/components/account/accountInfoForm.js
+++ src/components/account/accountInfoForm.js
@@ -11,27 +11,61 @@ class AccountInfoForm extends Component {
 
     return (
       <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
-          <Field className='sign-in-form__email' 
+          <Field className='account-info-form__name' 
+          type='name' 
+          title='Name' 
+          placeholder='Name' 
+          name='name' 
+          component={FormInput}/>
+          
+          <Field className='account-info-form__email' 
           type='email' 
           title='Email' 
           placeholder='Email' 
           name='email' 
           component={FormInput}/>
 
-          <Field className='sign-in-form__password' 
+          <Field className='account-info-form__street-address' 
+          type='address' 
+          title='Street Address' 
+          placeholder='Street Address' 
+          name='address' 
+          component={FormInput}/>
+
+          <Field className='account-info-form__city' 
+          type='city' 
+          title='City' 
+          placeholder='City' 
+          name='city' 
+          component={FormInput}/>
+
+          <Field className='account-info-form__state' 
+          type='state' 
+          title='State' 
+          placeholder='State' 
+          name='state' 
+          component={FormInput}/>
+
+          <Field className='account-info-form__zipcode' 
+          type='zipcode' 
+          title='Zip Code' 
+          placeholder='Zip Code' 
+          name='zipcode' 
+          component={FormInput}/>
+
+          {/* <Field className='account-info-form__password' 
           type='password' 
           title='Password' 
           placeholder='Password' 
           name='password' 
           component={FormInput}/>
-          <div className="sign-in-form__line"></div>
 
-          <Field className='sign-in-form__login' 
+          <Field className='account-info-form__login' 
           onClick={() => history.push('/account')}
           type='submit' 
           title='Login'  
           name='login' 
-          component={FormButton}/>
+          component={FormButton}/> */}
       </form>
     );
   }
