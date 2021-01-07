d208135189e6204d2271fbaee99d0f1ff00dca8a
Madison Creese <mads@Madisons-MacBook-Pro.local>
2021-01-06 19:09:54 -0800

    finished cart-footer component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
+import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
 import ShopCart from './shopCart';
 
 class Shop extends Component {
+
     constructor() {
         super()
 
@@ -23,9 +24,10 @@ class Shop extends Component {
                 path: '/signin'
             }
         ]
-
         this.props.setHeaderLinks(headerLinks);
         this.props.fetchShopCategories();
+
+        // filter products with links
         this.props.fetchShopProducts();
     }
 
@@ -41,33 +43,38 @@ class Shop extends Component {
     }
 
     render() {
+        <ShopCart className='shop__cart'/>
+
         return (
-        <div className="shop">
-            <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
-            <div className="shop__products">
+            <div className='shop'>
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+                <div className='shop__products'>
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <ShopProduct {...product} key={product._id} />
+                            )
+                        })
+                    }
+                </div>
                 {
-                    this.props.filteredProducts.map(product => {
-                        return (
-                            <ShopProduct {...product} key={product._id} />
-                        )
-                    })
+                    this.state.showCart ? <ShopCart className='shop__cart'/> : ''
                 }
+                
+                {/* shop cart button */}
             </div>
-            {
-                this.state.showCart ? <ShopCart className='shop__cart'/> : ''
-            }
-        </div>
-        );
+        )
     }
 }
 
 function mapStateToProps(state) {
-    const { categories, filteredProducts } = state.shop
+    const { categories, filteredProducts } = state.shop;
     return {
         categories,
         filteredProducts
-    }
+    } 
 }
 
 Shop = connect(mapStateToProps, actions)(Shop);
+
 export default Shop;
