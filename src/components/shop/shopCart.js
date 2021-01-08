8498e47e5921ba88892a24e28cdbd9784520fa69
Madison Creese <mads@Madisons-MacBook-Pro.local>
2021-01-07 19:05:15 -0800

    calculating order subtotal and total

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -11,6 +11,7 @@ import history from '../../history';
 function CartContent({className, products}) {
     let count = products.length;
     let productsJSX = products.map(product => <CartProduct {...product} key={product._id}/>);
+    
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
@@ -23,8 +24,12 @@ function CartContent({className, products}) {
         </div>
     )
 }
+
 function CartFooter({className, products}) {
-    const price = 7.96;
+    let subtotal = 0;
+    products.map(cartProduct => {
+        subtotal += cartProduct.quantity * cartProduct.product.price;
+    })
     return (
         <div className={`${className} cart-footer`}>
             <a onClick = {() => history.push('/order/review')} className='cart-footer__checkout'>
@@ -34,7 +39,7 @@ function CartFooter({className, products}) {
                 Subtotal
             </div>
             <div className='cart-footer__price'>
-                ${price}
+                ${subtotal}
             </div>
         </div>
     )
