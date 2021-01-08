6595e8e8389bf3d355c702c1921d63d4828507bc
Madison Creese <mads@Madisons-MacBook-Pro.local>
2021-01-07 18:50:14 -0800

    built review form details

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -12,8 +12,12 @@ class ReviewForm extends Component {
 
     return (
       <form onSubmit={handleSubmit} className={`${className} review-form`}>
+        <div className='review-form__legend'>
+          <div className='review-form__name'>Name</div>
+          <div className='review-form__quantity'>Quantity</div>
+          <div className='review-form__price'>Price</div>
+        </div>
         <ReviewProducts className='review-form__products' />
-        
         <div className='review-form__line'></div>
         <Field className='review-form__proceed' 
         onClick={() => history.push('/signin')}
@@ -29,6 +33,28 @@ class ReviewForm extends Component {
         name='back' 
         short = {true}
         component={FormButton}/>
+
+        <div className='review-form__details review-details'>
+          <div className='review-details__subtotal review-detail'>
+            <div className='review-detail__title'>Subtotal</div>
+            <div className='review-detail__price'>$7.96</div>
+          </div>
+
+          <div className='review-details__subtotal review-detail'>
+            <div className='review-detail__title'>Tax</div>
+            <div className='review-detail__price'>$0.16</div>
+          </div>
+
+          <div className='review-details__subtotal review-detail'>
+            <div className='review-detail__title'>Shipping</div>
+            <div className='review-detail__price'>$0.00</div>
+          </div>
+
+          <div className='review-details__total review-detail review-detail-green'>
+            <div className='review-detail__title review-detail-green__title'>Total</div>
+            <div className='review-detail__price review-detail-green__price'>$8.12</div>
+          </div>
+        </div>
       </form>
     );
   }
