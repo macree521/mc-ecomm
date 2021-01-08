a4d1262662f98fce16b9e7411d3aada42a708596
Madison Creese <mads@Madisons-MacBook-Pro.local>
2021-01-07 17:28:13 -0800

    built review product component

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -65,49 +65,56 @@ export function fetchShopProducts() {
                 title: 'Javascript in the Browser',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 1]
+                belongsTo : [0, 1],
+                imageURL: 'http://via.placeholder.com/80x80'
             },
             {
                 _id: 1,
                 title: 'Graph Database',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 5]
+                belongsTo : [0, 5],
+                imageURL: 'http://via.placeholder.com/80x80'
             },
             {
                 _id: 2,
                 title: 'Full Stack Development',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 1, 4]
+                belongsTo : [0, 1, 4],
+                imageURL: 'http://via.placeholder.com/80x80'
             },
             {
                 _id: 3,
                 title: 'User Interface Design',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 2]
+                belongsTo : [0, 2],
+                imageURL: 'http://via.placeholder.com/80x80'
             },
             {
                 _id: 4,
                 title: 'JavaScript Development',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 1]
+                belongsTo : [0, 1],
+                imageURL: 'http://via.placeholder.com/80x80'
             },
             {
                 _id: 5,
                 title: 'User Experience Design',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 2]
+                belongsTo : [0, 2],
+                imageURL: 'http://via.placeholder.com/80x80'
             },
             {
                 _id: 6,
                 title: 'Advanced OOP',
                 description: 'Investing time to improve your listening skills really pays off: The best listeners enjoy stronger relationships at and outside of work, are far more productive, and boast high confidence levels, just to name a few.',
                 price: 1.99,
-                belongsTo : [0, 6]
+                belongsTo : [0, 6],
+                imageURL: 'http://via.placeholder.com/80x80'
             }
         
         ]
